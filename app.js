78653754a0ee34fd80fdc1d8951b0681a4113f7b
Fajer Alhamad <fajer335544@hotmail.com>
2023-03-27 11:36:15 +0200

    initialize Pharmacy  Warehouse Router Controller

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hi");
+const express=require('express');
+const Ph_Router=require('./Router/Pharmacy_router');
+const Warehouse_Router=require('./Router/Warehouse_router');
+const app=express();
+const bodyParser=require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS,DELETE,PATCH');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorizations');
+    next();
+    
+    
+    
+    });
+
+
+    app.use(Ph_Router);
+    app.use(Warehouse_Router);
+    app.listen(8080);
